ac911bb645e07eb81ad39e8a5b5a74bf2af2dc12
Max Pfandl <max@pfandl.name>
2020-12-09 12:07:46 +0100

    add backup to deploymentskript

--- publishTtrss.cmd
+++ publishTtrss.cmd
@@ -2,6 +2,9 @@ REM rmdir /q /s \\192.168.0.15\docker\rssfeed\bin\
 REM dotnet publish -c Release -o \\192.168.0.15\docker\rssfeed\bin\
 REM c:\Data\publishfolder\
 
+
+wsl rsync -a -v ttrss:/var/dotnet/whiskydb/wwwroot/* /mnt/d/Documents/Development/whiskyserverapp/DataBackup/
+
 rmdir /q /s d:\Documents\Development\publishfolder\
 dotnet publish -c Release -o d:\Documents\Development\publishfolder\ -r linux-x64 --self-contained false
 rmdir /q /s d:\Documents\Development\publishfolder\wwwroot\images\
