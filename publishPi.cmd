b92b5307c3e0352e06d036d10ee61085bbdc3b66
Max Pfandl <max@pfandl.name>
2020-12-30 18:39:35 +0100

    add local deployment

---
+++ publishPi.cmd
@@ -0,0 +1,14 @@
+REM rmdir /q /s \\192.168.0.15\docker\rssfeed\bin\
+REM dotnet publish -c Release -o \\192.168.0.15\docker\rssfeed\bin\
+REM c:\Data\publishfolder\
+
+
+wsl rsync -a -v piServer:/var/dotnet/whiskydb/wwwroot/images/* /mnt/d/Documents/Development/whiskydb/DataBackup/images
+wsl rsync -a -v piServer:/var/dotnet/whiskydb/app.db /mnt/d/Documents/Development/whiskydb/DataBackup/app.db
+
+rmdir /q /s d:\Documents\Development\publishfolder\
+dotnet publish -c Release -o d:\Documents\Development\publishfolder\ -r ubuntu-arm64
+
+wsl rsync -a -v /mnt/d/Documents/Development/publishfolder/ piServer:/var/dotnet/whiskydb/
+
+ssh piServer '/home/ubuntu/deployscripts/whisky.sh'
